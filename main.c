da4920c52875a57eabbae3aea90dc48f7e0a18b9
kybex11 <artm.22@internet.ru>
2024-06-07 20:42:57 +0300

    update

--- main.c
+++ main.c
@@ -1,28 +1,31 @@
-#define TABLE_SIZE_X 210
-#define TABLE_SIZE_Y 210
-#define TABLE_SIZE_Z 250
+#define TABLE_SIZE_X                 210
+#define TABLE_SIZE_Y                 210
+#define TABLE_SIZE_Z                 250
 
-#define MAX_TABLE_TEMP 110
-#define MAX_HOTEND_TEMP 260
+#define MAX_TABLE_TEMP               110
+#define MAX_HOTEND_TEMP              260
 
-#define START_COORD_X 0
-#define START_COORD_Y 0
-#define START_COORD_Z 0
+#define START_COORD_X                0
+#define START_COORD_Y                0
+#define START_COORD_Z                0
 
-#define CURRENT_COORD_X 0
-#define CURRENT_COORD_Y 0
-#define CURRENT_COORD_Z 0
+#define CURRENT_COORD_X              0
+#define CURRENT_COORD_Y              0
+#define CURRENT_COORD_Z              0
 
-#define ENDSTOP_SYS_ON true
+#define ENDSTOP_SYS_ON               true
 
-#define ENDSTOP_X false
-#define ENDSTOP_Y false
-#define ENDSTOP_Z false
+#define ENDSTOP_X                    false
+#define ENDSTOP_Y                    false
+#define ENDSTOP_Z                    false
 
 #define AUTO_SCAN_COORD_AFTER_LAUNCH true
 
-// don't recommended change the defines
-#define STEP_PIN 2
-#define DIR_PIN 3
-#define STEP_MAX_SPEED 150
-#define STEP_ACCELERATION 75
+// don't recommended change the defines 
+#define STEP_PIN            2
+#define DIR_PIN             3
+#define STEP_MAX_SPEED      150
+#define STEP_ACCELERATION   75
+#define TFT_CS              10
+#define TFT_RST             9
+#define TFT_DC              8
--- src/screen/screen.c
+++ src/screen/screen.c
@@ -1,46 +1,15 @@
-#include <stdio.h>
-#include <stdint.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <linux/fb.h>
-#include <sys/mman.h>
-#include <sys/ioctl.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ILI9341.h>
+#include <SPI.h>
+#include "main.c"
 
-#define FBDEV "/dev/fb1"
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_RST);
 
-int main() {
-    int fbfd = 0;
-    struct fb_var_screeninfo vinfo;
-    uint16_t *fbp;
-
-    fbfd = open(FBDEV, O_RDWR);
-    if (fbfd == -1) {
-        perror("Error: cannot open framebuffer device");
-        return 1;
-    }
-
-    if (ioctl(fbfd, FBIOGET_VSCREENINFO, &vinfo)) {
-        perror("Error reading variable information");
-        close(fbfd);
-        return 1;
-    }
-
-    fbp = (uint16_t *)mmap(0, vinfo.yres_virtual * vinfo.xres_virtual * 2, PROT_READ | PROT_WRITE, MAP_SHARED, fbfd, 0);
-    if ((int)fbp == -1) {
-        perror("Error: failed to map framebuffer device to memory");
-        close(fbfd);
-        return 1;
-    }
-
-    // Draw "Hello World" text on the screen
-    int x = 100, y = 100;
-    uint16_t color = 0xF800; // Red color
-    for (int i = 0; i < 11; i++) {
-        fbp[(y + 0) * vinfo.xres_virtual + x + i] = color;
-    }
-
-    munmap(fbp, vinfo.yres_virtual * vinfo.xres_virtual * 2);
-    close(fbfd);
-
-    return 0;
+void setup() {
+  tft.begin();
+  tft.setRotation(3);
+  tft.setTextSize(2);
+  tft.setTextColor(ILI9341_WHITE);
+  tft.setCursor(100, 100); 
+  tft.println("Hello World");
 }
